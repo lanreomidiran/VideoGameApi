153bf0354c5fb0a179ab00f5c96e77c42adc8fd9
Lanre Omidiran <lanreomidiran@gmail.com>
2026-04-17 22:09:31 +0100

    VideoGameController

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Scalar.AspNetCore;
+using VideoGameApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<IVideoGameCharacterService, VideoGameCharacterService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Services/VideoGameCharacterService.cs
@@ -0,0 +1,36 @@
+﻿using VideoGameApi.Models;
+
+namespace VideoGameApi.Services
+{
+    public class VideoGameCharacterService : IVideoGameCharacterService
+    {
+        static List<Character> characters = new List<Character>
+        {
+            new Character { Id = 1, Name = "Mario", Game = "Super Mario Bros.", Role = "Hero" },
+            new Character { Id = 2, Name = "Link", Game = "The Legend of Zelda", Role = "Hero" },
+            new Character { Id = 3, Name = "Bowser", Game = "Super Mario Bros", Role = "Villain" }
+        };
+        public Task<Character> AddCharacterAsync(Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCharacterAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Character>> GetAllCharactersAsync()
+         => await Task.FromResult(characters);
+
+        public Task<Character> GetCharacterByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCharacterAsync(int id, Character character)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
