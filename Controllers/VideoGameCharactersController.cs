98d9411e2b853dc901c0d36d259e10aec9a05e71
Lanre Omidiran <lanreomidiran@gmail.com>
2026-04-17 21:00:18 +0100

    first commit

---
+++ Controllers/VideoGameCharactersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameCharactersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCharacters() 
+        {
+            var characters = new[] 
+            {
+
+            }
+        }
+    }
+}
