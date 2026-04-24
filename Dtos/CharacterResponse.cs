3a97ccab89feefb70639ca66ac5c33ddd17fd018
Lanre Omidiran <lanreomidiran@gmail.com>
2026-04-24 07:34:12 +0100

    DTO

--- Dtos/CharacterResponse.cs
+++ Dtos/CharacterResponse.cs
@@ -2,5 +2,9 @@
 {
     public class CharacterResponse
     {
+        // public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Game { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
     }
 }
--- Services/VideoGameCharacterService.cs
+++ Services/VideoGameCharacterService.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using VideoGameApi.Data;
+using VideoGameApi.Dtos;
 using VideoGameApi.Models;
 
 namespace VideoGameApi.Services
 {
     public class VideoGameCharacterService(AppDbContext context) : IVideoGameCharacterService
     {
-        public Task<Character> AddCharacterAsync(Character character)
+        public Task<CharacterResponse> AddCharacterAsync(Character character)
         {
             throw new NotImplementedException();
         }
@@ -16,16 +18,44 @@ namespace VideoGameApi.Services
             throw new NotImplementedException();
         }
 
-        public async Task<List<Character>> GetAllCharactersAsync()
-         => await context.Characters.ToListAsync();
+        public async Task<List<CharacterResponse>> GetAllCharactersAsync()
+         => await context.Characters.Select(c=> new CharacterResponse
+         {
+             Name = c.Name,
+             Game = c.Game,
+             Role = c.Role,
+         }).ToListAsync();
 
-        public async Task<Character?> GetCharacterByIdAsync(int id)
+        public async Task<CharacterResponse?> GetCharacterByIdAsync(int id)
         {
-            var result = await context.Characters.FindAsync(id);
+            var result = await context.Characters
+                .Where(c => c.Id == id)
+                .Select(c => new CharacterResponse
+                {
+                    Name = c.Name,
+                    Game = c.Game,
+                    Role = c.Role,
+                })
+                .FirstOrDefaultAsync();
             return result;
         }
 
         public Task<bool> UpdateCharacterAsync(int id, Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<CharacterResponse> IVideoGameCharacterService.AddCharacterAsync(Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<CharacterResponse>> IVideoGameCharacterService.GetAllCharactersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<CharacterResponse?> IVideoGameCharacterService.GetCharacterByIdAsync(int id)
         {
             throw new NotImplementedException();
         }
